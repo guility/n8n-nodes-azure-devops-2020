7ad6d9a07dddf2d6c0d16fe289637de19c28033c
Guility <andrey.vinovniy@gmail.com>
2025-09-22 12:30:48 +0700

    Added .prettierrc and .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier'
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index'
+        ],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc' }
+      }
+    ]
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {}
+    }
+  }
+};
